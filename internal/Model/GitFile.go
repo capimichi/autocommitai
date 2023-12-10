7ba0509c58165afa2191cc4b709661f43b166b64
Michele Capichioni <capimichi@gmail.com>
2023-12-10 12:09:28 +0100

    Implement IsDir, IsTracked, GetPath, and GetStatus methods in GitFile

---
+++ internal/Model/GitFile.go
@@ -0,0 +1,34 @@
+package Model
+
+import "os"
+
+type GitFile struct {
+	Path   string
+	Status string
+}
+
+func NewGitFile(path string, status string) *GitFile {
+	return &GitFile{Path: path, Status: status}
+}
+
+
+func (gf *GitFile) IsDir() bool {
+	filePath := gf.Path
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		return false
+	}
+	return fileInfo.IsDir()
+}
+
+func (gf *GitFile) IsTracked() bool {
+	return gf.Status != "?" && gf.Status != "??"
+}
+
+func (gf *GitFile) GetPath() string {
+	return gf.Path
+}
+
+func (gf *GitFile) GetStatus() string {
+	return gf.Status
+}
