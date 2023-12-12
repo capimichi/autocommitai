42a066c3b936119a81d122a27a1ff0cb7b1b285c
Michele Capichioni <capimichi@gmail.com>
2023-12-12 23:21:03 +0100

    Add AutoCommitCommand struct

---
+++ internal/Command/AutoCommitCommand.go
@@ -0,0 +1,59 @@
+package Command
+
+import (
+	"autocommitai/internal/Config"
+	"autocommitai/internal/Service"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func AutoCommitCommand() *cobra.Command {
+
+	defaultChoiceKey := "default-choice"
+	ignoreUntrackedKey := "ignore-untracked"
+
+	cmd := &cobra.Command{
+		Use:   "autocommit",
+		Short: "AutoCommitAI is a tool to automatically commit your code",
+		Run: func(cmd *cobra.Command, args []string) {
+			defaultConfig := Config.NewDefaultConfig()
+			err := defaultConfig.InitConfig()
+
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			autoCommitAiService := Service.NewAutoCommitAiService()
+
+			ignoreUntracked, err := cmd.Flags().GetBool(ignoreUntrackedKey)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			autoCommitAiService.SetIgnoreUntracked(ignoreUntracked)
+
+			defaultChoice, err := cmd.Flags().GetString(defaultChoiceKey)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			autoCommitAiService.SetDefaultChoice(defaultChoice)
+
+			err = autoCommitAiService.Execute()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		},
+	}
+
+	cmd.Flags().Bool(ignoreUntrackedKey, false, "Ignore untracked files when committing")
+	cmd.Flags().String(defaultChoiceKey, "", "Default choice for commit message (1. commit, 2. commit with custom message, 3. ignore, 4. skip)")
+
+	return cmd
+}
